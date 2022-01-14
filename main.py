f29de29864ad640f7fb0475e437223c9d153d4d3
Christian Kimura <christiankimura94@gmail.com>
2022-01-14 08:20:08 -0600

    changing it to be competitive against the computer

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import random
 min = 1
 max = 6
 
-new_roll = input("Type ROLL to throw the dice.\n")
+computer_die = random.randint(min, max)
+user_die = random.randint(min, max)
 
-if new_roll == "ROLL":
-    print("You Rolled...\n")
-    print(random.randint(min, max));
+new_roll = input("Type 'roll' to throw the dice.\n")
 
+if new_roll == "roll":
+    print("You Rolled...")
+    print(user_die);
+    print("The Computer Rolled...")
+    print(computer_die)
+
+    if user_die > computer_die:
+        print("\n YOU WIN!")
+    else:
+        print("\n You Lose :(")
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-Dice rolling application. Text based user interface(TUI)
+v1. Dice rolling application. Text based user interface(TUI).
+
+v2. Adding competitive aspect:
+    You roll Dice, Computer rolls dice, determines winner, outputs you win or you lose.
